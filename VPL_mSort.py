2a35548ab2b360a8ba3af36ae7f94931a61f86eb
ruybragafilho <ruybragafilho@gmail.com>
2021-09-28 10:26:17 -0300

    Commit inicial

---
+++ VPL_mSort.py
@@ -0,0 +1,45 @@
+def head(L):
+    return L[0]
+ 
+def tail(L):
+    return L[1]
+
+def py2ll(L):
+    # TODO
+    return None
+
+def ll2py(L):
+    # TODO
+    return []
+
+def size(L):
+    # TODO
+    return 0
+
+def sorted(L):
+    # TODO
+    return False
+
+def sum(L):
+    # TODO
+    return 0
+
+def split(L):
+    # TODO
+    return None
+
+def merge(L0, L1):
+    # TODO
+    return None
+
+def mSort(L):
+    # TODO
+    return None
+
+def max(L):
+    # TODO
+    return 0
+
+def get(L, N):
+    # TODO
+    return None
